60722fdef696cc39ad165e9760e575877184a010
bobjrsenior <bobjrsenior@gmail.com>
2016-12-18 09:37:35 -0500

    Added start of message processing. Can identify a bit message from a non-bit message. Plan for the irc/processing to be a pipeline thing (sockerRead->parseMessage->Game deals with parsed message) in order for easier multi-game implementation and to be able to switch to ssl sockets easier.

--- Assets/Scripts/Twitch/IrcTest.cs
+++ Assets/Scripts/Twitch/IrcTest.cs
@@ -10,18 +10,23 @@ public class IrcTest : MonoBehaviour {
 
     private bool continueSock = true;
 
-    private Thread sockThread;
+    //private Thread sockThread;
 
     private List<string> messages = new List<string>();
 
-    private bool lockList = false;
+    private bool lockMessageList = false;
+
+    private List<string> bitMessages = new List<string>();
+
+    private bool lockBitMessageList = false;
 
     // Use this for initialization
     void Start () {
 
-        sockThread = new Thread(new ThreadStart(StartSocket));
-        sockThread.Start();
+        //sockThread = new Thread(new ThreadStart(StartSocket));
+        //sockThread.Start();
         //StartCoroutine(StartSocketCoRoutine());
+        StartCoroutine(ParseMessageList());
 	}
 	
 	// Update is called once per frame
@@ -29,19 +34,38 @@ public class IrcTest : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             continueSock = false;
-            sockThread.Abort();
+            //sockThread.Abort();
         }
 
-        if (!lockList)
+        if (Input.GetKeyDown(KeyCode.Return))
         {
-            lockList = true;
+            if (!lockMessageList)
+            {
+                lockMessageList = true;
+                if (System.DateTime.Now.Ticks % 2 == 0)
+                {
+                    messages.Add("@badges=staff/1,bits/1000;bits=100;color=;display-name=TWITCH_UserNaME;emotes=;id=b34ccfc7-4977-403a-8a94-33c6bac34fb8;mod=0;room-id=1337;subscriber=0;turbo=1;user-id=1337;user-type=staff :twitch_username!twitch_username@twitch_username.tmi.twitch.tv PRIVMSG #channel :cheer100");
+                }
+                else
+                {
+                    messages.Add("@badges=global_mod/1,turbo/1;color=#0D4200;display-name=TWITCH_UserNaME;emotes=25:0-4,12-16/1902:6-10;mod=0;room-id=1337;subscriber=0;turbo=1;user-id=1337;user-type=global_mod :twitch_username!twitch_username@twitch_username.tmi.twitch.tv PRIVMSG #channel :Kappa Keepo Kappa");
+                }
+                lockMessageList = false;
+            }
+        }
+
+        /*
+        if (!lockMessageList)
+        {
+            lockMessageList = true;
             if(messages.Count > 0)
             {
                 print(messages[0]);
                 messages.RemoveAt(0);
             }
-            lockList = false;
+            lockMessageList = false;
         }
+        */
 
     }
 
@@ -61,10 +85,10 @@ public class IrcTest : MonoBehaviour {
             string message = IRCHandler.ReadSocket(socket);
             if (message.Trim().Length > 1)
             {
-                while (lockList) { Thread.Sleep(10); }
-                lockList = true;
+                while (lockMessageList) { Thread.Sleep(10); }
+                lockMessageList = true;
                 messages.Add(message);
-                lockList = false;
+                lockMessageList = false;
             }
             Thread.Sleep(100);
 
@@ -93,16 +117,48 @@ public class IrcTest : MonoBehaviour {
             string message = IRCHandler.ReadSocket(socket);
             if (message.Trim().Length > 1)
             {
-                while (lockList) { yield return new WaitForSeconds(0.01f); }
-                lockList = true;
+                while (lockMessageList) { yield return new WaitForSeconds(0.01f); }
+                lockMessageList = true;
                 messages.Add(message);
-                lockList = false;
+                lockMessageList = false;
             }
             yield return new WaitForSeconds(.1f);
         } while (continueSock && socket.Connected);
-
+        continueSock = false;
 
         socket.Close();
     }
 
+    IEnumerator ParseMessageList()
+    {
+        do
+        {
+            if (!lockMessageList && !lockBitMessageList)
+            {
+                string message = "";
+                lockMessageList = true;
+                lockBitMessageList = true;
+                if(messages.Count > 0)
+                {
+                    message = messages[0];
+                    messages.RemoveAt(0);
+                }
+                lockMessageList = false;
+                if (!message.Equals("")){
+                    string[] messageSplit = message.Split(':');
+                    if(messageSplit.Length >= 2 && messageSplit[0].Contains(";bits="))
+                    {
+                        print(messageSplit[0]);
+
+                        string[] stats = messageSplit[0].Split(';');
+                    }
+                }
+                lockBitMessageList = false;
+            }
+            yield return new WaitForSeconds(.1f);
+        } while (continueSock);
+
+
+        socket.Close();
+    }
 }
